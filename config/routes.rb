808c95b8f522de291419984203b076ae53ca34f8
bino2121 <akatuki2121332@gmail.com>
2021-11-29 04:14:19 +0000

    Generate user registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'user/new'
   get '/posts/index' => "posts#index"
   get '/posts/new' => 'posts#new'
   get '/posts/:id' => "posts#show"
   post 'posts/create' => "posts#create"
+  get "/users/new" => "users#new"
   get '/' => 'home#top'
   #root 'application#goodbye'
 end
